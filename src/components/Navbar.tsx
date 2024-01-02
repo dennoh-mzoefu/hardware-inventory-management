006fff05913a1c438c3c64f69b1a5df257335a8e
dennoh-mzoefu <denisgichana162@gmail.com>
2024-01-02 18:00:00 +0200

    component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <div className='w-[80vw] md:w-[65vw] mx-auto flex justify-between'>
+            <p></p>
+            {/* avatar */}
+            <p className='w-7 h-7 text-center flex justify-center text-white items-center my-4 rounded-full bg-primary shadow-md'>D</p>
+        </div>
+    )
+}
+
+export default Navbar
