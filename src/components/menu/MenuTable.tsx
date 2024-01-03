7c01a964d13119107218b90e206a430d20339664
dennoh-mzoefu <denisgichana162@gmail.com>
2024-01-03 18:00:00 +0200

    menu component

---
+++ src/components/menu/MenuTable.tsx
@@ -0,0 +1,371 @@
+import React, { useEffect, useState } from 'react';
+import {
+    ChevronLeftIcon,
+    ChevronRightIcon,
+    DoubleArrowLeftIcon,
+    DoubleArrowRightIcon,
+} from "@radix-ui/react-icons"
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select"
+import {
+    ColumnDef,
+    ColumnFiltersState,
+    SortingState,
+    VisibilityState,
+    flexRender,
+    getCoreRowModel,
+    getFilteredRowModel,
+    getPaginationRowModel,
+    getSortedRowModel,
+    useReactTable,
+} from "@tanstack/react-table"
+import {
+    ArrowUpDown, ChevronDown, MoreHorizontal, FilePlus,
+    FileEdit,
+    Trash2,
+    ShoppingCart,
+} from "lucide-react"
+
+import { Button } from "@/components/ui/button"
+import { Checkbox } from "@/components/ui/checkbox"
+import {
+    DropdownMenu,
+    DropdownMenuCheckboxItem,
+    DropdownMenuContent,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Input } from "@/components/ui/input"
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table"
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area"
+import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { firebaseDateFormated } from '@/utils/date';
+import { addCurrentOrder } from '@/redux/OrderSlice';
+import { ToastContainer, toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+
+export type Stock = {
+    id: string
+    productName: string
+    sizeOrCategory: string
+    quantity: number
+    buyingPrice: number
+    discount: number
+    sellingPrice: number
+    location: string
+    reorderPoint: number
+    dateAdded: Date
+    lastRestocked: Date
+}
+
+
+export function MenuTable() {
+    const notifySuccess = () =>
+        toast.success("item Added", {
+            position: "top-right",
+            autoClose: 1000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+        });
+    const notifyError = () =>
+        toast.error("Error Occured", {
+            position: "top-right",
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "colored",
+        });
+    const { preOrder } = useSelector((state: any) => state.order);
+    const dispatch = useDispatch()
+    const handleOrder = (stockData: any) => {
+        try {
+            !preOrder.map((x) => x.id).includes(stockData.id) && dispatch(addCurrentOrder({ ...stockData, orderId: stockData.productName + " " + stockData.sizeOrCategory }))
+            notifySuccess()
+            console.log({ stockData })
+        } catch (error) {
+            notifyError()
+        }
+    }
+    const columns: ColumnDef<Stock>[] = [
+        {
+            accessorKey: "productName",
+            header: ({ column }) => {
+                return (
+                    <Button
+                        variant="ghost"
+                        onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+                    >
+                        Name
+                        <ArrowUpDown className="ml-2 h-4 w-4" />
+                    </Button>
+                )
+            },
+            cell: ({ row }) => (
+                <div className="capitalize">{row.getValue("productName")}</div>
+            ),
+        },
+        {
+            accessorKey: "sizeOrCategory",
+            header: ({ column }) => {
+                return (
+                    <Button
+                        variant="ghost"
+                        onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+                    >
+                        Category
+                        <ArrowUpDown className="ml-2 h-4 w-4" />
+                    </Button>
+                )
+            },
+            cell: ({ row }) => (
+                <div className="capitalize">{row.getValue("sizeOrCategory")}</div>
+            ),
+        },
+        {
+            accessorKey: "quantity",
+            header: () => <div className="text-right">Quantity</div>,
+            cell: ({ row }) => <div className="lowercase">{row.getValue("quantity")}</div>,
+        },
+        {
+            accessorKey: "discount",
+            header: () => <div className="text-right">Discount</div>,
+            cell: ({ row }) => <div className="lowercase">{row.getValue("discount")}</div>,
+        },
+        {
+            accessorKey: "sellingPrice",
+            header: () => <div className="text-right">Unit Price</div>,
+            cell: ({ row }) => <div className="lowercase">{row.getValue("sellingPrice")}</div>,
+        },
+        {
+            id: "actions",
+            enableHiding: false,
+            cell: ({ row }) => {
+                // const payment = row.original
+                const stockData = row.original
+
+                return (
+                    <Button
+                        variant="default"
+                        onClick={() => handleOrder(stockData)}
+                    >
+                        Order
+                        <ShoppingCart className="ml-2 h-4 w-4" />
+                    </Button>
+                )
+            },
+        },
+    ]
+
+    const { stock } = useSelector((state: any) => state.stock);
+
+    const data: Stock[] = stock
+
+    const [sorting, setSorting] = React.useState<SortingState>([])
+    const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
+        []
+    )
+    const [columnVisibility, setColumnVisibility] =
+        React.useState<VisibilityState>({})
+    const [rowSelection, setRowSelection] = React.useState({})
+
+    const table = useReactTable({
+        data,
+        columns,
+        onSortingChange: setSorting,
+        onColumnFiltersChange: setColumnFilters,
+        getCoreRowModel: getCoreRowModel(),
+        getPaginationRowModel: getPaginationRowModel(),
+        getSortedRowModel: getSortedRowModel(),
+        getFilteredRowModel: getFilteredRowModel(),
+        onColumnVisibilityChange: setColumnVisibility,
+        onRowSelectionChange: setRowSelection,
+        state: {
+            sorting,
+            columnFilters,
+            columnVisibility,
+            rowSelection,
+        },
+    })
+    return (
+        <div className="sm:w-[95vw] md:w-[50.5vw]">
+            <ToastContainer />
+            <div className="flex md:w-6/6 items-center py-4">
+                <Input
+                    placeholder="Search product name"
+                    value={(table.getColumn("productName")?.getFilterValue() as string) ?? ""}
+                    onChange={(event) =>
+                        table.getColumn("productName")?.setFilterValue(event.target.value)
+                    }
+                    className="max-w-sm sm:mr-2 md:mr-0"
+                />
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button variant="outline" className="ml-auto">
+                            Columns <ChevronDown className="ml-2 h-4 w-4" />
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end">
+                        {table
+                            .getAllColumns()
+                            .filter((column) => column.getCanHide())
+                            .map((column) => {
+                                return (
+                                    <DropdownMenuCheckboxItem
+                                        key={column.id}
+                                        className="capitalize"
+                                        checked={column.getIsVisible()}
+                                        onCheckedChange={(value) =>
+                                            column.toggleVisibility(!!value)
+                                        }
+                                    >
+                                        {column.id}
+                                    </DropdownMenuCheckboxItem>
+                                )
+                            })}
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            </div>
+            <ScrollArea className="whitespace-nowrap rounded-md border">
+                <div className="rounded-md border">
+                    <Table className="w-full">
+                        <TableHeader>
+                            {table.getHeaderGroups().map((headerGroup) => (
+                                <TableRow key={headerGroup.id}>
+                                    {headerGroup.headers.map((header) => {
+                                        return (
+                                            <TableHead key={header.id}>
+                                                {header.isPlaceholder
+                                                    ? null
+                                                    : flexRender(
+                                                        header.column.columnDef.header,
+                                                        header.getContext()
+                                                    )}
+                                            </TableHead>
+                                        )
+                                    })}
+                                </TableRow>
+                            ))}
+                        </TableHeader>
+                        <TableBody>
+                            {table.getRowModel().rows?.length ? (
+                                table.getRowModel().rows.map((row) => (
+                                    <TableRow
+                                        key={row.id}
+                                        data-state={row.getIsSelected() && "selected"}
+                                    >
+                                        {row.getVisibleCells().map((cell) => (
+                                            <TableCell key={cell.id} className='text-center'>
+                                                {flexRender(
+                                                    cell.column.columnDef.cell,
+                                                    cell.getContext()
+                                                )}
+                                            </TableCell>
+                                        ))}
+                                    </TableRow>
+                                ))
+                            ) : (
+                                <TableRow>
+                                    <TableCell
+                                        colSpan={columns.length}
+                                        className="h-24 text-center"
+                                    >
+                                        No results.
+                                    </TableCell>
+                                </TableRow>
+                            )}
+                        </TableBody>
+                    </Table>
+                </div>
+                <ScrollBar orientation="horizontal" />
+            </ScrollArea>
+            <div className="flex items-center justify-end space-x-2 py-4 sm:w-[80vw] md:w-[50.5vw] flex-wrap">
+                <div className="flex md:items-center md:justify-between px-2">
+
+                    <div className="flex items-center space-x-4 lg:space-x-8 flex-wrap sm:justify-end">
+                        <div className="flex items-center space-x-2">
+                            <p className="text-sm font-medium">Rows per page</p>
+                            <Select
+                                value={`${table.getState().pagination.pageSize}`}
+                                onValueChange={(value) => {
+                                    table.setPageSize(Number(value))
+                                }}
+                            >
+                                <SelectTrigger className="h-8 w-[70px]">
+                                    <SelectValue placeholder={table.getState().pagination.pageSize} />
+                                </SelectTrigger>
+                                <SelectContent side="top">
+                                    {[10, 20, 30, 40, 50].map((pageSize) => (
+                                        <SelectItem key={pageSize} value={`${pageSize}`}>
+                                            {pageSize}
+                                        </SelectItem>
+                                    ))}
+                                </SelectContent>
+                            </Select>
+                        </div>
+                        <div className="flex w-fit md:w-[100px] items-center justify-center text-sm font-medium">
+                            Page {table.getState().pagination.pageIndex + 1} of{" "}
+                            {table.getPageCount()}
+                        </div>
+                        <div className="flex items-center space-x-2">
+                            <Button
+                                variant="outline"
+                                className="hidden h-8 w-8 p-0 lg:flex"
+                                onClick={() => table.setPageIndex(0)}
+                                disabled={!table.getCanPreviousPage()}
+                            >
+                                <span className="sr-only">Go to first page</span>
+                                <DoubleArrowLeftIcon className="h-4 w-4" />
+                            </Button>
+                            <Button
+                                variant="outline"
+                                className="h-8 w-8 p-0"
+                                onClick={() => table.previousPage()}
+                                disabled={!table.getCanPreviousPage()}
+                            >
+                                <span className="sr-only">Go to previous page</span>
+                                <ChevronLeftIcon className="h-4 w-4" />
+                            </Button>
+                            <Button
+                                variant="outline"
+                                className="h-8 w-8 p-0"
+                                onClick={() => table.nextPage()}
+                                disabled={!table.getCanNextPage()}
+                            >
+                                <span className="sr-only">Go to next page</span>
+                                <ChevronRightIcon className="h-4 w-4" />
+                            </Button>
+                            <Button
+                                variant="outline"
+                                className="hidden h-8 w-8 p-0 lg:flex"
+                                onClick={() => table.setPageIndex(table.getPageCount() - 1)}
+                                disabled={!table.getCanNextPage()}
+                            >
+                                <span className="sr-only">Go to last page</span>
+                                <DoubleArrowRightIcon className="h-4 w-4" />
+                            </Button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
