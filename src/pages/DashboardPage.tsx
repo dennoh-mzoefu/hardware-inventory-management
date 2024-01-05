c680cc10c73cd90f61d07e813add9bda3a0d7f8a
dennoh-mzoefu <denisgichana162@gmail.com>
2024-01-05 18:00:00 +0200

    dashboard page

---
+++ src/pages/DashboardPage.tsx
@@ -0,0 +1,13 @@
+import Dashboard from '@/components/dashboard/dashboard'
+import React from 'react'
+
+function DashboardPage() {
+    return (
+        <div>
+            <Dashboard
+            />
+        </div>
+    )
+}
+
+export default DashboardPage
