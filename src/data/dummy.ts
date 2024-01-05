b75730c3aaf8ef5230e1e47411142732967e89a3
dennoh-mzoefu <denisgichana162@gmail.com>
2024-01-05 18:00:00 +0200

    data

---
+++ src/data/dummy.ts
@@ -0,0 +1,58 @@
+// export type Playlists = (typeof playlists)[number];
+
+export const playlists = [
+  "Recently Added",
+  "Recently Played",
+  "Top Songs",
+  "Top Albums",
+  "Top Artists",
+  "Logic Discography",
+  "Bedtime Beats",
+  "Feeling Happy",
+  "I miss Y2K Pop",
+  "Runtober",
+  "Mellow Days",
+  "Eminem Essentials",
+];
+
+export const links: any = {
+  pages: [
+    {
+      title: "Dashboard",
+      link: "dashboard",
+      icon: "coffee",
+    },
+    {
+      title: "Menu",
+      link: "",
+      icon: "coffee",
+    },
+    {
+      title: "Sales",
+      link: "sales",
+      icon: "coffee",
+    },
+    {
+      title: "Expenses",
+      link: "expense",
+      icon: "coffee",
+    },
+    {
+      title: "Stock",
+      link: "stock",
+      icon: "coffee",
+    },
+  ],
+  report: [
+    {
+      title: "sales report",
+      link: "dashboard",
+      icon: "coffee",
+    },
+    {
+      title: "expense report",
+      link: "dashboard",
+      icon: "coffee",
+    },
+  ],
+};
