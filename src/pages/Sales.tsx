32ad1812fb37788ceb202875298c181ad793e901
dennoh-mzoefu <denisgichana162@gmail.com>
2024-01-06 18:00:00 +0200

    sales page

---
+++ src/pages/Sales.tsx
@@ -0,0 +1,14 @@
+// import { DataTableDemo } from '@/components/stock/StockTable'
+
+import { SaleTable } from "@/components/sale/SaleTable"
+
+
+function Sales() {
+    return (
+        <div>
+            <SaleTable className="mx-auto" />
+        </div>
+    )
+}
+
+export default Sales
