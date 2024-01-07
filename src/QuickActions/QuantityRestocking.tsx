86dd2a6a4a9e058ff84758253ace791a08510cb2
dennoh-mzoefu <denisgichana162@gmail.com>
2024-01-07 18:00:00 +0200

    quick actions page

---
+++ src/QuickActions/QuantityRestocking.tsx
@@ -0,0 +1,12 @@
+import RestockingTable from '@/components/actions/stock/RestockingTable'
+import React from 'react'
+
+function QuantityRestocking() {
+    return (
+        <div>
+            <RestockingTable />
+        </div>
+    )
+}
+
+export default QuantityRestocking
