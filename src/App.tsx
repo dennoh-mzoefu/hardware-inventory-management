6149e615f7f312ae6cd45a0a362b3a922ee21eff
dennoh-mzoefu <denisgichana162@gmail.com>
2024-12-30 18:00:00 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import PostApp from './PostApp'
+import PreApp from './PreApp'
+
+function App() {
+
+  return (
+    <div>
+      <PreApp />
+      <PostApp />
+    </div>
+  )
+}
+
+export default App
